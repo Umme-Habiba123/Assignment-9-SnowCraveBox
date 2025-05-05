34fe4baf17382113cc7ac4b8653805b9136f9854
umme-habiba123 <habibaislammim@gmail.com>
2025-05-05 18:46:43 +0600

    added subscription card

--- src/Components/SubscriptionCard.jsx
+++ src/Components/SubscriptionCard.jsx
@@ -1,19 +1,25 @@
-import React, { use } from 'react';
+import React, {  useEffect, useState } from 'react';
 import { Link } from 'react-router';
 import SubscribeItem from '../Pages/SubscribeItem';
 
-const subscriptionPromise = fetch('/public/catDog.json').then(res => res.json())
+// const subscriptionPromise = fetch('/catDog.json').then(res => res.json())
 
 const SubscriptionCard = () => {
-    console.log(subscriptionPromise)
-    const subscriptions = use(subscriptionPromise)
+    const [subscription, setSubscription]=useState([])
+    // console.log(subscriptionPromise)
+
+    useEffect(()=>{
+        fetch('/catDog.json').then(res=>res.json())
+        .then(data=>setSubscription(data))
+    },[])
+    // const subscriptions = use(subscriptionPromise)
     return (
         <div className='w-10/12 mx-auto'>
-            <h1 className='text-4xl text-center my-15 text-gray-500 satisfy-font'>Be Part of Our Community – Join Now! {subscriptions.length}</h1>
+            <h1 className='text-4xl text-center my-15 text-gray-500 satisfy-font'>Be Part of Our Community – Join Now! {subscription.length}</h1>
 
             <div className='grid grid-cols-3'>
                 {
-                    subscriptions.map((item => <Link to={`/SubscribeItem/${item.id}`} key={item.id}><SubscribeItem item={item}></SubscribeItem> </Link>))
+                    subscription.map((item => <Link to={`/SubscribeItem/${item.id}`} key={item.id}><SubscribeItem item={item}></SubscribeItem> </Link>))
                 }
 
                 
--- src/Pages/SubscribeItem.jsx
+++ src/Pages/SubscribeItem.jsx
@@ -1,24 +1,24 @@
 import React from 'react';
 
-const SubscribeItem = (item) => {
-    const [thumbnail] = item
+const SubscribeItem = ({item}) => {
+    const {thumbnail,name,description} = item
     return (
-        <div>
+        <div className=''>
             <div className="card bg-base-100 w-96 shadow-sm">
                 <figure>
                     <img
                         src={thumbnail}
-                        alt="Shoes" />
+                        alt="" />
                 </figure>
                 <div className="card-body">
                     <h2 className="card-title">
-                        Card Title
+                        {name}
                         <div className="badge badge-secondary">NEW</div>
                     </h2>
-                    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                    <p>{description}</p>
                     <div className="card-actions justify-end">
-                        <div className="badge badge-outline">Fashion</div>
-                        <div className="badge badge-outline">Products</div>
+                        <div className="badge badge-outline">Subscribe</div>
+                        <div className="badge badge-outline">Details</div>
                     </div>
                 </div>
             </div>
